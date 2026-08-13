48e58c7c13bbf1a25ffae1a72aab71a4623146ab
Anandhu <anandhu.jayakumar@imageinfosystems.com>
2026-08-13 15:43:32 +0530

    high valu report

---
+++ src/com/iispl/reporting/HighValueReportRunner.java
@@ -0,0 +1,43 @@
+package com.iispl.reporting;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+public class HighValueReportRunner {
+
+	public static void generateHighValueReport() {
+    	
+    	try {
+    		Connection connection = DBConnection.getConnection();
+    		
+    		String jrxml = "report/high_value_report.jrxml" ;
+    		
+    		String pdf = "output/high_value_report.pdf";
+    		
+    		JasperReport report = JasperCompileManager.compileReport(jrxml);
+    		
+    		Map<String, Object> parameters = new HashMap<>();
+    		
+    		JasperPrint print = JasperFillManager.fillReport(report, parameters, connection);
+    		
+    		JasperExportManager.exportReportToPdfFile(print, pdf);
+    		
+    		connection.close();
+    		
+            System.out.println("High Value report generated successfully!");
+
+    		
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+	
+
+}
